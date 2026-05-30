1206e1302acbcc708b838dc71623c36479268bc5
jcm-tx <hustle1272@gmail.com>
2026-05-29 20:12:55 -0500

    Update route.ts

--- src/app/api/twilio/inbound/route.ts
+++ src/app/api/twilio/inbound/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/prefer-regexp-exec */
+
 // src/app/api/twilio/inbound/route.ts
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
